31db74158623b246b290cf2411f29d82cc4ac4c3
Gondogan <barbitasupc@gmail.com>
2026-04-15 20:39:59 +0200

    Clases Hijas de Characters y Paquete Skills

--- proyecto2_Berserk/src/characters/Casca.java
+++ proyecto2_Berserk/src/characters/Casca.java
@@ -1,5 +1,40 @@
 package characters;
 
-public class Casca {
+import skills.Ataque;
+import skills.HabilidadEspecial;
 
+public class Casca extends Personajes {
+
+    public Casca() {
+        super("Casca", 100, 20, 15, 20, 60);
+    }
+
+    @Override
+    protected void inicializarHabilidades() {
+        // 1. Habilidad Pasiva
+        this.getHabilidades().add(new HabilidadPasiva("Supervivencia Instintiva", 
+            "Pasiva: Al recibir un golpe letal, sobrevive con 1 HP (Solo funciona una vez por combate)."));
+            
+        // 2. Ataque Básico (Coste: 0)
+        this.getHabilidades().add(new Ataque("Golpe Táctico", 
+            "Corte preciso buscando los puntos débiles de la armadura.", 1.0));
+            
+        // 3. Habilidad Especial (Coste: 15)
+        this.getHabilidades().add(new HabilidadEspecial("Patada Desequilibrante", 
+            "Golpea al enemigo dejándolo 'Vulnerable' para el siguiente turno.", 1.3, 15));
+            
+        // 4. Habilidad Especial Definitiva (Coste: 35)
+        this.getHabilidades().add(new HabilidadEspecial("Carga de la Vanguardia", 
+            "Ataque coordinado con una ferocidad implacable.", 2.4, 35));
+    }
+
+    @Override
+    protected void aplicarBonusDeSubidaNivel() {
+        // Casca es la más equilibrada en todas las áreas
+        this.setVidaMaxima(this.getVidaMaxima() + 15);
+        this.setDanioBase(this.getDanioBase() + 3);
+        this.setDefensa(this.getDefensa() + 3);
+        this.setVelocidad(this.getVelocidad() + 3);
+        this.setEnergiaMaxima(this.getEnergiaMaxima() + 8);
+    }
 }
--- proyecto2_Berserk/src/characters/Guts.java
+++ proyecto2_Berserk/src/characters/Guts.java
@@ -1,5 +1,39 @@
 package characters;
 
-public class Guts {
+import skills.Ataque;
+import skills.HabilidadEspecial;
 
+public class Guts extends Personajes {
+
+    public Guts() {
+        super("Guts", 120, 30, 20, 10, 50);
+    }
+
+    @Override
+    protected void inicializarHabilidades() {
+    	// 1. Habilidad Pasiva
+        this.getHabilidades().add(new HabilidadPasiva("Marca del Sacrificio", 
+            "Pasiva: Si la vida de Guts baja del 30%, entra en estado 'Berserker' y hace un 50% más de daño."));
+            
+        // 2. Ataque Básico (Coste: 0)
+        this.getHabilidades().add(new Ataque("Tajo Matadragones", 
+            "Un barrido horizontal muy pesado.", 1.2));
+            
+        // 3. Habilidad Especial (Coste: 25)
+        this.getHabilidades().add(new HabilidadEspecial("Corte Ciclón", 
+            "Gira sobre sí mismo dañando con gran fuerza.", 1.8, 25));
+            
+        // 4. Habilidad Especial Definitiva (Coste: 45)
+        this.getHabilidades().add(new HabilidadEspecial("Cañón de Brazo", 
+            "Dispara el cañón oculto en su prótesis a quemarropa. Daño masivo.", 3.5, 45));
+    }
+
+    @Override
+    protected void aplicarBonusDeSubidaNivel() {
+        // Guts prioriza Vida, Ataque y Defensa
+        this.setVidaMaxima(this.getVidaMaxima() + 20);
+        this.setDanioBase(this.getDanioBase() + 6);
+        this.setDefensa(this.getDefensa() + 5);
+        this.setEnergiaMaxima(this.getEnergiaMaxima() + 5);
+    }
 }
